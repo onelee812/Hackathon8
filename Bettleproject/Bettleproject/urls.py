5d1121cff18b96099719140d900cb383be55783e
Austin <austin.jiuk.kim@mgmail.com>
2022-05-28 18:38:46 +0900

    Add API

--- Bettleproject/Bettleproject/urls.py
+++ Bettleproject/Bettleproject/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bettleApp import views
+from user import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home', views.home, name="home"),
-    path('signup', views.signup, name="signup"),
-    path('logout', view.logout name="logout"),
-    path('teamadd', views.teamadd, name="teamadd"),
-    path('teamlist', views.teamlist, name="teamlist"),
-    path('notice', views.notice, name="notice"),
+    path("admin/", admin.site.urls),
+    # path("home", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("logout/", views.logout, name="logout"),
+    # path("teamadd", views.teamadd, name="teamadd"),
+    # path("teamlist", views.teamlist, name="teamlist"),
+    # path("notice", views.notice, name="notice"),
 ]
