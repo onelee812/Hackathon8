7f698ff8bfcd20e17d41ce3ac735bba31f6de2e2
Park747 <qkrrusdn747@korea.ac.kr>
2022-05-28 18:57:29 +0900

    1

--- Bettleproject/bettleApp/views.py
+++ Bettleproject/bettleApp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required 
+from .models import Team
 # Create your views here.
+
+@login_required(login_url="/user/login")
+def teamadd(request): 
+    if request.method == "POST":
+        team = Team.objects.create(
+            hostUser = request.user,
+            firstMember = request.POST["firstMember"],
+            secondMember = request.POST["secondMember"],
+            thirdMember = request.POST["thirdMember"],
+            fourthMember = request.POST["fourthMember"],
+            Tier = request.POST["Tier"],
+            bulletBet = request.POST["bulletBet"],
+        )
+        match_dates = request.POST.getlist('match_dates[]')
+        team.set_matchDates(match_dates)
+        print('날짜정보', team.get_matchDates())
+        return redirect('teamlist')
+    return render(request, 'teamadd.html')
+
+
+def teamlist(request):
+    teams = Team.objects.all()
+    return render(request, 'teamlist.html', {'teams' : teams})
+
